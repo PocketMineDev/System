662f0a76bc9956e388f39a17c5b6c60b3519fff2
PocketMineDev <43330168+PocketMineDev@users.noreply.github.com>
2018-12-08 10:45:35 +0100

    Update

--- src/Cb/SBTask.php
+++ src/Cb/SBTask.php
@@ -3,20 +3,22 @@
 namespace Cb;
 
 use pocketmine\scheduler\Task;
-use Cb\Manager;
+use Cb\System;
 use pocketmine\utils\Config;
 
 class SBTask extends Task {
 	
     public $plugin;
     
-	public function __construct(Manager $plugin){
+	public function __construct(System $plugin){
 		$this->plugin = $plugin;
 	}
 	
 	public function onRun(int $currentTick) : void {
         $pl = $this->plugin->getServer()->getOnlinePlayers();
         foreach ($pl as $player) {
+        	$pp = $this->plugin->getServer()->getPluginManager()->getPlugin("PurePerms");
+            $group = $pp->getUserDataMgr()->getGroup($player, null);
             $name = $player->getName();
             $ping = $player->getPing();
             $server = "CityBuild";
@@ -25,11 +27,14 @@ class SBTask extends Task {
             $this->plugin->rmScoreboard($player, "objektName");
             $this->plugin->createScoreboard($player, "§7*§aMelonenGames§7*", "objektName");
             $this->plugin->setScoreboardEntry($player, 1, "", "objektName");
-            $this->plugin->setScoreboardEntry($player, 2, "§aName§7:", "objektName");
+            $this->plugin->setScoreboardEntry($player, 2, "§aUserName§7:", "objektName");
             $this->plugin->setScoreboardEntry($player, 3, "§7$name", "objektName");
             $this->plugin->setScoreboardEntry($player, 4, " ", "objektName");
-            $this->plugin->setScoreboardEntry($player, 5, "§aPing§7:", "objektName");
-            $this->plugin->setScoreboardEntry($player, 6, "§7$ping", "objektName");
+            $this->plugin->setScoreboardEntry($player, 5, "§aKontostand§7:", "objektName");
+            $this->plugin->setScoreboardEntry($player, 6, $this->plugin->getServer()->getPluginManager()->getPlugin('EconomyAPI')->myMoney($player), "objektName");
+            $this->plugin->setScoreboardEntry($player, 7, "  ", "objektName");
+            $this->plugin->setScoreboardEntry($player, 8, "§aRang", "objektName");
+            $this->plugin->setScoreboardEntry($player, 9, $group, "objektName");
 
         }
 	}
